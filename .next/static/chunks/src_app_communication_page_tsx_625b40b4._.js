5b7dae0187e2cf610f867c4dec2efab3adfd2c9e
ng-mahesh <124774279+ng-mahesh@users.noreply.github.com>
2025-07-21 12:47:00 +0530

    Add .next/static/chunks/src_app_communication_page_tsx_625b40b4._.js 1753082220631

---
+++ .next/static/chunks/src_app_communication_page_tsx_625b40b4._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_14f0ca10._.js",
+  "static/chunks/node_modules_56d9ba72._.js"
+],
+    source: "dynamic"
+});
