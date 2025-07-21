da4fcd148957b860dbe82aca8bd075f84f21af2c
ng-mahesh <124774279+ng-mahesh@users.noreply.github.com>
2025-07-21 12:46:47 +0530

    Add .next/static/chunks/src_app_complaints_page_tsx_625b40b4._.js 1753082207925

---
+++ .next/static/chunks/src_app_complaints_page_tsx_625b40b4._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_99f47741._.js",
+  "static/chunks/node_modules_27f63f14._.js"
+],
+    source: "dynamic"
+});
