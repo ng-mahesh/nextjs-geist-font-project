5cc28f4b225040b6bb3e6b3e2ab79963d56802cc
ng-mahesh <124774279+ng-mahesh@users.noreply.github.com>
2025-07-21 12:47:01 +0530

    Add .next/static/chunks/src_app_residents_page_tsx_625b40b4._.js 1753082221769

---
+++ .next/static/chunks/src_app_residents_page_tsx_625b40b4._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d5ef23e1._.js",
+  "static/chunks/src_3041b6fa._.js"
+],
+    source: "dynamic"
+});
